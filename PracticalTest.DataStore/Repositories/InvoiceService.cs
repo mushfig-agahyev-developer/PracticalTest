8018bdbfd54b913ad006767bfb28b87a5ee96090
mushfig-agahyev-developer <webprogrammer.readonly@gmail.com>
2022-09-03 15:11:26 +0400

    part_1

--- PracticalTest.DataStore/Repositories/InvoiceService.cs
+++ PracticalTest.DataStore/Repositories/InvoiceService.cs
@@ -1,6 +1,8 @@
-﻿using PracticalTest.DataStore.DTO;
+﻿using PracticalTest.DataStore.DAL;
+using PracticalTest.DataStore.DTO;
 using PracticalTest.DataStore.Interfaces;
 using PracticalTest.DataStore.InvoiceCalculate;
+using PracticalTest.DataStore.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,15 @@ namespace PracticalTest.DataStore.Repositories
 {
     public class InvoiceService : IInvoiceService
     {
+        private readonly AppStore _db;
+        public Message _message { get; set; }
+        public InvoiceService(AppStore dbContext)
+        {
+            _message = new Message();
+            _message.Notifications = new List<Notification>();
+            _db = dbContext;
+        }
+
         public Task<BaseInvoiceDetails> DetailsInvoices(InvoiceDetails model)
         {
             throw new NotImplementedException();
--- PracticalTest.DataStore/Repositories/LoanService.cs
+++ PracticalTest.DataStore/Repositories/LoanService.cs
@@ -1,6 +1,8 @@
-﻿using PracticalTest.DataStore.DTO;
+﻿using PracticalTest.DataStore.DAL;
+using PracticalTest.DataStore.DTO;
 using PracticalTest.DataStore.Interfaces;
 using PracticalTest.DataStore.InvoiceCalculate;
+using PracticalTest.DataStore.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,15 @@ namespace PracticalTest.DataStore.Repositories
 {
     public class LoanService : ILoanService
     {
+        private readonly AppStore _db;
+        public Message _message { get; set; }
+        public LoanService(AppStore dbContext)
+        {
+            _message = new Message();
+            _message.Notifications = new List<Notification>();
+            _db = dbContext;
+        }
+
         public Task<LoanDto> Add(LoanDto loanModel)
         {
             throw new NotImplementedException();
